c4606ee334e6753e8411ef2b79c2f74359d7910d
Konstantin Makarov <kossmak@gmail.com>
2025-11-24 22:21:26 +0300

    Extend parameters of cli wrapper

--- _cli.py
+++ _cli.py
@@ -1,15 +1,25 @@
+
 import uvicorn
 
-import src.main
+# import src.main
+# from src.core.configs import settings
+# from main import app
 
 
-
-if __name__ == "__main__":
+def _main() -> None:
     uvicorn.run(
+        # "main:app",  # зависит от рабочего каталога
         "src.main:app",
+        # host="0.0.0.0",
         host="127.0.0.1",
         port=8088,
         reload=True,
-        reload_excludes=["*.pyc", "__pycache__", "templates"],
+        log_level="debug",
+        reload_dirs=["src"],
+        # reload_excludes=["*.pyc", "__pycache__", "templates"],
+        reload_excludes=["*.pyc", "__pycache__", "templates", "src/templates"],
     )
 
+
+if __name__ == "__main__":
+    _main()
